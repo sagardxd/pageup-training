6ebd42234ff31ca28cfcd81d264556d2a0501036
Sagar Yadav <sagardxd5@gmail.com>
2024-07-30 18:16:11 +0530

    added

--- 7/http-learn/src/app/services/auth-interceptor.service.ts
+++ 7/http-learn/src/app/services/auth-interceptor.service.ts
@@ -1,11 +1,13 @@
 import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable, tap } from "rxjs";
+import { environment } from "../../environments/environment";
 
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const modifedReq = req.clone({ headers: req.headers.append('auth-Header', 'xyz') });
         console.log('Request is on its way');
+        console.log(environment.apiUrl);
 
         return next.handle(modifedReq).pipe(tap(event => {
             if (event.type === HttpEventType.Response) {
